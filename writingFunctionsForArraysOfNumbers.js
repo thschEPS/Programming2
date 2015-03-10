1ec39f297a39b40db5b157316104ace87652f47d
EPSTCross <tcross@eastsideprep.org>
2015-03-09 18:39:36 -0700

    Add branch for assignment

---
+++ writingFunctionsForArraysOfNumbers.js
@@ -0,0 +1,10 @@
+function biggestNumberInArray(arrayToCheck) {
+	//write a function that returns the largest number in an array
+}
+
+//write another function that returns the average of the numbers in an array
+//rounded to two decimal places
+
+var jennysNumber = [8, 6, 7, 5, 3, 0, 9];
+console.log(biggestNumberInArray(jennysNumber));
+console.log(averageOfNumbersInArray(jennysNumber));
