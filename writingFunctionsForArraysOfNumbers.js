95d6ab5b054713e081c2570b3d6de92774ae4944
EPSTCross <tcross@eastsideprep.org>
2015-03-09 19:50:10 -0700

    Add a boilerplate index.html file and instructions

--- writingFunctionsForArraysOfNumbers.js
+++ writingFunctionsForArraysOfNumbers.js
@@ -1,10 +1,15 @@
-function biggestNumberInArray(arrayToCheck) {
-	//write a function that returns the largest number in an array
+//Some of you may have never seen or written in Javascript. Others are fresh
+//off of a trimester of doing nothing but. To start, we'll start to get the
+//hang of both some basic JS syntax and GitHub use. Open the index.html in
+//your favorite browser and get started.
+
+function largestNumberInArray(arrayToCheck) {
+	//1. Write a function that returns the largest number in an array
 }
 
-//write another function that returns the average of the numbers in an array
+//2. Write another function that returns the average of the numbers in an array
 //rounded to two decimal places
 
 var jennysNumber = [8, 6, 7, 5, 3, 0, 9];
-console.log(biggestNumberInArray(jennysNumber));
+console.log(largestNumberInArray(jennysNumber));
 console.log(averageOfNumbersInArray(jennysNumber));
