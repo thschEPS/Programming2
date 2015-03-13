5bea3402a907c5038851303bef86e95d8e822a6b
thschEPS <thschregalo@gmail.com>
2015-03-13 10:28:25 -0700

    Second Commit

--- writingFunctionsForArraysOfNumbers.js
+++ writingFunctionsForArraysOfNumbers.js
@@ -1,15 +1,23 @@
-//Some of you may have never seen or written in Javascript. Others are fresh
-//off of a trimester of doing nothing but. To start, we'll start to get the
-//hang of both some basic JS syntax and GitHub use. Open the index.html in
-//your favorite browser and get started.
-
-function largestNumberInArray(arrayToCheck) {
-	//1. Write a function that returns the largest number in an array
+function findNumber() {
+  var jNum = [8, 6, 7, 5, 3, 0, 9];
+  biggestNumberInArray(jNum);
+  arrayAverage(jNum);
 }
-
-//2. Write another function that returns the average of the numbers in an array
-//rounded to two decimal places
-
-var jennysNumber = [8, 6, 7, 5, 3, 0, 9];
-console.log(largestNumberInArray(jennysNumber));
-console.log(averageOfNumbersInArray(jennysNumber));
+function biggestNumberInArray(jNum) {
+  var bigNum = 0; 
+  for (var i = 0; i < jNum.length; i++) {
+    if (jNum[i] > bigNum) {
+      bigNum = jNum[i];     
+    }
+  }
+  console.log(bigNum);
+}
+function arrayAverage(jNum) {
+  var sum = 0; 
+  for (var i = 0; i < jNum.length; i++) {
+    sum = jNum[i] + sum;
+  }
+  sum = (sum/(jNum.length)).toFixed(2);
+  console.log(sum);
+}
+findNumber();
